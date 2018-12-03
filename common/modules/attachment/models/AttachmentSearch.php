cc8c9972bb6880a87d09b2c84f655c46ea22b95d
JieChengYang <2064320087@qq.com>
2018-12-03 10:25:54 +0800

    附件分页bug修改

--- common/modules/attachment/models/AttachmentSearch.php
+++ common/modules/attachment/models/AttachmentSearch.php
@@ -22,19 +22,13 @@ class AttachmentSearch extends Attachment
         $field = 'id';
         $sort = SORT_DESC;
 
-        // if (isset($params['pageSize'])) {
-        //     $pageSize = $params['pageSize'];
+        if (isset($getParams['page'])) {
+            $pageCurrent = $getParams['page'] - 1;
+        }
 
-        //     $pageCurrent = $params['pageCurrent']-1;
-
-        //     if (in_array(($params['orderField']), ['id', 'created_at', 'updated_at', 'created_at_format', 'created_at_format'])) {
-        //         $field = $params['orderField'];
-        //     }
-
-        //     if (strtolower($params['orderDirection']) == 'asc') {
-        //         $sort = SORT_ASC;
-        //     }
-        // }
+        if (isset($getParams['pageSize'])) {
+            $pageSize = $getParams['pageSize'];
+        }
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,

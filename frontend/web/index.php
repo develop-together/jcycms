41c48dbba20bbc92aeb0671b44a899be27244b78
Yjc <2064320087@qq.com>
2019-05-31 20:56:31 +0800

    修改忽略文件

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -1,7 +1,10 @@
 <?php
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
-
+// if (YII_DEBUG) {
+// 	error_reporting(E_ALL);
+// 	ini_set('display_errors', 'On');	
+// }
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
 require __DIR__ . '/../../common/config/bootstrap.php';

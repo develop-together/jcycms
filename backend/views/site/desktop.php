6b6595e67d688cc5d9aff302f8ca78d9a7c3ed9b
杨捷成 <yangjiecheng@boyuntong.com>
2019-06-02 15:40:42 +0800

    修改邮箱配置

--- backend/views/site/desktop.php
+++ backend/views/site/desktop.php
@@ -103,7 +103,7 @@
                     <?php foreach ($commentRanking as $key => $cr): ?>
                         <li class="list-group-item">
                             <?php
-                                if (0 === $key $read->comment_count > 0) {
+                                if (0 === $key && $read->comment_count > 0) {
                                     echo  Html::a($cr->title, Url::to(['article/view', 'id' => $cr->id]), ['title' => $cr->sub_title]) . '<small class="pull-right text-danger"><i class="fa fa-heart"></i></small> <span class="pull-right badge badge-info">' . $cr->comment_count . '</span> ';
                                 } else {
                                     echo Html::a($cr->title, Url::to(['article/view', 'id' => $cr->id]), ['title' => $cr->sub_title]) . '<span class="pull-right badge badge-info">' . $cr->comment_count . '</span>';
